79b5867cbbc4949cd604cae29aaf6adcac4d009c
M. HELMAN ANSYARI <helmanansyari99@gmail.com>
2023-02-16 20:05:52 +0800

    perubahan carousel 2

--- src/Component/Carousel.js
+++ src/Component/Carousel.js
@@ -5,39 +5,50 @@ function Carousel1(props) {
     <Carousel>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block w-100 h-100"
           src={props.image1}
           alt="First slide"
         />
         <Carousel.Caption>
           <h3>{props.judul1}</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block w-100 h-100"
           src={props.image2}
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>{props.judul2}</h3>
+          
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block w-100 h-100"
           src={props.image3}
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h3>{props.judul3}</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100 h-100"
+          src={props.image4}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>{props.judul4}</h3>
+          
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
--- src/Component/Dropdown.js
+++ src/Component/Dropdown.js
@@ -1,8 +1,12 @@
 import { Container, Row, Col, Accordion } from "react-bootstrap";
 import Carousel from './Carousel'
-import gambar from '../assets/2.png'
+import gambar1 from '../assets/service-lainya.jpg'
 import gambar2 from '../assets/service-hp.jpg'
 import gambar3 from '../assets/service-pc.jpg'
+import gambar4 from '../assets/html.png'
+import gambar5 from '../assets/css.png'
+import gambar6 from '../assets/js.png'
+import gambar7 from '../assets/react.svg'
 
 function Dropdown() {
     return (
@@ -11,17 +15,17 @@ function Dropdown() {
             <h2>Berikut beberapa skill yang saya milki.</h2>
               <Row>
                   <Col className='col-12 px-0 rounded-2 shadow' >
-                  <Accordion defaultActiveKey="0">
+                  <Accordion>
                         <Accordion.Item eventKey="0">
                           <Accordion.Header className='accbg'>Service Electronika</Accordion.Header>
                           <Accordion.Body>
-                          <Carousel image1={gambar} image2={gambar2} image3={gambar3} judul1={'tes'}/>
+                          <Carousel image1={gambar2} image2={gambar3} image3={gambar1} judul1={'SERIVE HP'} judul2={'SERVICE LAPTOP/KOMPUTER'} judul3={'SERVICE ELEKTRONIK LAINYA'}/>
                           </Accordion.Body>
                         </Accordion.Item>
                         <Accordion.Item eventKey="1">
-                          <Accordion.Header>Programing (html, css, javascript)</Accordion.Header>
+                          <Accordion.Header>Programing (html, css, javascript, React)</Accordion.Header>
                           <Accordion.Body>
-                          <Carousel />
+                          <Carousel image1={gambar4} image2={gambar5} image3={gambar6} image4={gambar7} judul1={'HTML'} judul2={'CSS'} judul3={'JAVASCRIPT'} judul4={'REACT'}/>
                           </Accordion.Body>
                         </Accordion.Item>
                     </Accordion>
